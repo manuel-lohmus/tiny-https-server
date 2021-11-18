3f4e5913b065bacba3e9bdadaffb7cb887676c50
manuel <manuel@hauss.ee>
2021-11-18 21:28:03 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,105 @@
+# tiny-webs-erver: Easy tiny https web server middleware.
+
+[![npm-version](https://badgen.net/npm/v/tiny-https-server)](https://www.npmjs.com/package/tiny-https-server)
+[![npm-week-downloads](https://badgen.net/npm/dw/tiny-https-server)](https://www.npmjs.com/package/tiny-https-server)
+
+Easy tiny https web server middleware.
+Easy to use and configure.
+Handle subdomains.
+Serving static files.
+
+## Installing
+
+`npm install tiny-https-server`
+
+## Usage example
+
+app.js
+```js
+"use strict";
+
+if (require("try-to-run")()) {
+    // isMainThread
+    require('log-report').clear();
+    return;
+}
+
+console.time("Time");
+var options = require("config-sets").tiny_https_server;
+options.subdomains = { test: "./public/test" };
+
+var server = require("tiny-https-server");
+// Console log
+server.on("request", function (req, res, next) {
+    console.timeLog("Time", `Url: ${req.headers.host}${req.url}`);
+    next();
+});
+// Test page.
+server.on("request", function (req, res, next) {
+    if (req.url === "/test") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write("Test page.");
+        res.end();
+    }
+    else
+        next();
+});
+// Simulated crash ...
+server.on("request", function (req, res, next) {
+    if (req.url === "/crash") {
+        throw new Error("Simulated crash ...");
+    }
+    else
+        next();
+});
+
+
+var port = options.port === 443 ? "" : ":" + options.port;
+var urls = [
+    `http://localhost${port}/`,
+    `http://test.localhost${port}/`,
+    `http://localhost${port}/test`,
+    `http://test.localhost${port}/test`,
+    `http://localhost${port}/home`
+];
+
+// Opens the URLs in the default browser.
+while (urls.length) {
+    require("browse-url")(urls.shift());
+}
+```
+
+## Config-sets file
+
+config-sets.json
+```json
+{
+  "production": {
+    "tiny_https_server": {
+      "domain": "localhost",
+      "port": 443,
+      "document_root": "./public/www",
+      "directory_index": "index.html",
+      "pathToError_404": "./error_404.html",
+      "pathToPrivkey": "./cert/localhost-key.pem",
+      "pathToCert": "./cert/localhost-cert.pem",
+      "subdomains": {
+        "test": "./public/test"
+      }
+    }
+  },
+  "development": {
+    "tiny_https_server": {
+      "subdomains": {
+        "test": "./public/test"
+      }
+    }
+  }
+}
+```
+
+## License
+
+[MIT](LICENSE)
+
+Copyright (c) 2021 Manuel L&otilde;hmus <manuel@hauss.ee>
---
+++ server.js
@@ -0,0 +1,165 @@
+/** Web server functions. @preserve Copyright (c) 2021 Manuel Lõhmus. */
+"use strict";
+
+var options = require("config-sets").init({
+    tiny_https_server: {
+        domain: "localhost",
+        port: "",
+        document_root: "./public/www",
+        directory_index: "index.html",
+        pathToError_404: "./error_404.html",
+        pathToPrivkey: "",
+        pathToCert: "",
+        subdomains: {}
+    }
+}).tiny_https_server;
+
+var http = require("http");
+var https = require("https");
+var isSSL = options.pathToPrivkey !== "" && options.pathToCert !== "";
+var port = options.port || (isSSL ? 443 : 80);
+var path = require("path");
+var fs = require("fs");
+var mimeTypes = require(path.resolve(__dirname, "mimeTypes.js"));
+
+
+if (isSSL && port === 443) {
+
+    http.createServer(function (req, res) {
+        if (req.url.startsWith("/."))
+            // for .well-known/acme-challenge/
+            static_reqest(req, res)
+        else {
+            // redirect http to https
+            res.writeHead(302, { "Location": "https://" + req.headers["host"] + req.url }); 
+            res.end();
+        }
+    }).listen(80);
+}
+
+var server = isSSL && port !== 80
+    ? https.createServer({
+        key: fs.readFileSync(options.pathToPrivkey),
+        cert: fs.readFileSync(options.pathToCert)
+    })
+    : http.createServer();
+
+var emit = server.emit;
+server.emit = function () {
+
+    var args = arguments;
+
+    if (args[0] === "request") {
+
+        var requestArr = [];
+
+        if (server._events.request) {
+            if (typeof server._events.request === "function")
+                requestArr.push(server._events.request);
+            else
+                requestArr = requestArr.concat(server._events.request);
+        }
+
+        requestArr.push(static_reqest);
+
+        function next() {
+
+            if (requestArr.length) {
+
+                var fnRequest = requestArr.shift();
+                fnRequest(args[1], args[2], next);
+            }
+        }
+
+        next();
+    }
+    else
+        emit.apply(server, args);
+};
+
+//server.on("request", function (req, res, next) { next(); });
+//server.on("request", function (req, res) { static_reqest(req, res); });
+
+server.listen(port, function (err) {
+
+    if (err)
+        console.error(err)
+    else
+        console.log("Webserver port:" + port + " pid:" + process.pid);
+});
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function static_reqest(req, res) {
+
+    var filename = req.url;
+
+    if (!filename.length || filename.endsWith("/")) { filename += options.directory_index; }
+    if (filename.startsWith("/")) { filename = filename.substr(1); }
+
+    var subdomain = req.headers.host.replace(":" + options.port, "");
+    subdomain = subdomain.replace(options.domain, "");
+    if (subdomain.endsWith(".")) { subdomain = subdomain.substr(0, subdomain.length - 1); }
+    var document_root = subdomain === "" ? options.document_root : options.subdomains[subdomain];
+
+    if (document_root) {
+
+        filename = path.resolve(document_root, filename);
+        static_file(filename, res, function () {
+
+            filename = path.resolve(options.document_root, options.pathToError_404);
+            static_file(filename, res, function (exists) {
+
+                if (!exists) {
+
+                    not_found_content(req, res);
+                }
+            })
+        });
+    }
+    else {
+        not_found_content(req, res);
+    }
+}
+/**
+ * @param {string} filename
+ * @param {http.ServerResponse} res
+ * @param {()void} callback
+ */
+function static_file(filename, res, fn_not_found) {
+
+    fs.exists(filename, function (exists) {
+
+        if (exists) {
+
+            var mimeType = mimeTypes[path.extname(filename)];
+            if (!mimeType) mimeType = "";
+            res.writeHead(200, { "Content-Type": mimeType });
+            fs.createReadStream(filename).pipe(res);
+        }
+        else
+            fn_not_found();
+    });
+}
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function not_found_content(req, res) {
+
+    console.info("Warning: 404 Not Found", {
+        url: req.url,
+        ip: req.socket.remoteAddress,
+        userAgent: req.headers["user-agent"]
+    });
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("404 Not Found Url: " + req.url + "\n");
+    if (options.isDebug) {
+        res.write("Headers: " + JSON.stringify(req.headers, null, 2) + "\n");
+    }
+    res.end();
+}
+
+module.exports = server;
